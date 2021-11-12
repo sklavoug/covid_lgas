95e1d0c503e90b3a77cfc9c3bfb07bd4842c3bb0
sklavoug <93801496+sklavoug@users.noreply.github.com>
2021-11-12 20:27:12 +1100

    Vis complete
    Untidy code but final draft, allows for the creation of a .gif visualisation with the final map split into Sydney and Regional NSW.

--- lga_visualisation.py
+++ lga_visualisation.py
@@ -16,6 +16,8 @@ from urllib.request import urlopen
 import geopandas as gpd
 from shapely.geometry import polygon
 import matplotlib.pyplot as plt
+import imageio
+import os
 
 def extract_from_api(url):
     '''
@@ -48,21 +50,7 @@ def extract_from_api(url):
     
     return df
 
-# Extract a subset of the data. It's clear for our purposes (a map of COVID cases
-# in NSW) we don't really care about most of the data; our shape files are for
-# LGAs so we'll keep the LGA name field (and possibly the code in case name doesn't
-# line up), and we'll keep the date because this is going to be based on date. Otherwise
-# we don't need postcode/suburb, NSW Health have been kind enough to conform them to
-# LGAs already.
-url = 'https://data.nsw.gov.au/data/api/3/action/datastore_search?resource_id=21304414-1ff1-4243-a5d2-f52778048b29&limit=10'  
 
-# extract_from_api(url)
-
-# With our second query we've got only what we wanted and we've extracted all the data,
-# all 77,000 rows of it.
-url = 'https://data.nsw.gov.au/data/api/3/action/datastore_search_sql?sql=SELECT%20notification_date,lga_code19,lga_name19%20from%20%2221304414-1ff1-4243-a5d2-f52778048b29%22%20'
-
-df = extract_from_api(url)
 
 # Now we have our data, we just need to get our shape files so we can visualise the output.
 # Note that the shape files are static and sourced from the Australian Bureau of Statistics (ABS):
@@ -70,60 +58,79 @@ df = extract_from_api(url)
 # Note that this file includes LGAs for the entirety of Australia, but all we really care about is NSW
 # as that's where our data is from.
 
-# So we just clean the LGA file so it's only got the NSW LGAs in it.
-def clean_lgas():
-    lgas = gpd.read_file('LGA_2021_AUST_GDA2020.shp')
+def clean(df):
+    # So we just clean the LGA file so it's only got the NSW LGAs in it.
+    def clean_lgas():
+        lgas = gpd.read_file('LGA_2021_AUST_GDA2020.shp')
+        
+        lgas = lgas.loc[lgas['STE_NAME21'] == 'New South Wales']
+        
+        lgas.to_file("LGA_2021_NSW_GDA2020.shp")
     
-    lgas = lgas.loc[lgas['STE_NAME21'] == 'New South Wales']
+    # Interestingly, the map works but there's a huge part sticking out on the RHS. I'm fairly certain
+    # from googling that this is Lord Howe Island, however, looking into the min/max x/y axis values
+    # for the map suggests it's not its own LGA, and instead falls into 'Unincorporated NSW'.
+    lgas = gpd.read_file('LGA_2021_NSW_GDA2020.shp')
+        
+    # print(lgas.columns)
+    # print(lgas['geometry'].bounds)
     
-    lgas.to_file("LGA_2021_NSW_GDA2020.shp")
-
-# Interestingly, the map works but there's a huge part sticking out on the RHS. I'm fairly certain
-# from googling that this is Lord Howe Island, however, looking into the min/max x/y axis values
-# for the map suggests it's not its own LGA, and instead falls into 'Unincorporated NSW'.
-lgas = gpd.read_file('LGA_2021_NSW_GDA2020.shp')
+    to_remove = lgas.loc[lgas['geometry'].bounds['maxx'] > 157]
+    # print(to_remove['LGA_NAME21'])
     
-# print(lgas.columns)
-# print(lgas['geometry'].bounds)
-
-to_remove = lgas.loc[lgas['geometry'].bounds['maxx'] > 157]
-# print(to_remove['LGA_NAME21'])
-
-# That's a problem because Unincorporated NSW is also the far west of the state, which we definitely
-# don't want to lose. We can barely see the island on the map too since it's so small, so probably
-# best to just set the x limit on the map (same way as for matplotlib).
-
-# print(lgas['LGA_NAME21'])
+    # That's a problem because Unincorporated NSW is also the far west of the state, which we definitely
+    # don't want to lose. We can barely see the island on the map too since it's so small, so probably
+    # best to just set the x limit on the map (same way as for matplotlib).
     
-lgas['geometry'].plot(figsize=(20,8)).set_xlim(140,154)
-
-lgas = lgas[['LGA_CODE21','LGA_NAME21','geometry']]
-
-# Now to finish cleaning up our case data! With 77000 rows and only ~365 days of data, it looks
-# like they're reporting each case as a new line. That's kind of great as it's super granular,
-# but for our purposes we can roll it up to LGAs and just see a count of how many in each LGA
-# on each day. Here we go!
-
-# print(df.drop_duplicates(keep='first'))
-
-# Yup that looks much more reasonable, 8130 combinations. So let's group them together, reset
-# the index and then merge with our LGA shape file. Note that in this case I'm adding a dummy
-# column 'count' with values all 1 (as they're each 1 case) so groupby has something to work
-# from.
-
-df['count'] = 1
-df = df.groupby(['notification_date','lga_code19','lga_name19'])[['count']].count()
-# print(df)
-df = df.reset_index(drop=False)
-# print(df)
-
-df = df.sort_values(by='notification_date', ascending=True)
-
-max_color = df['count'].max()
-
-dates = list(df['notification_date'].unique())
-
-dates = dates[:1]
+    # print(lgas['LGA_NAME21'])
+        
+    # lgas['geometry'].plot(figsize=(20,8)).set_xlim(140,154)
+    
+    lgas = lgas[['LGA_CODE21','LGA_NAME21','geometry']]
+    
+    # NSW is tough to visualise! Most notably because the state is so huge but so much of the population
+    # is centred in Sydney and surrounding LGAs, my first run of the map basically showed nothing
+    # in the entirety of 2020, which is wrong -- there was COVID there, it was just all in Sydney. The
+    # best way to solve this problem is probably to split the map into two subplots, one for Sydney
+    # and one for the rest of NSW. I've made a quick CSV mapping file of the LGA_NAME21 column
+    # from the lgas dataset to either Sydney/Regional based on the Wikipedia article on NSW
+    # LGAs: https://en.wikipedia.org/wiki/Local_government_areas_of_New_South_Wales
+    
+    syd_non = pd.read_csv('lgas_sydney_regional.csv')
+    
+    lgas = pd.merge(lgas, syd_non, how='inner', on='LGA_NAME21')
+    
+    print(lgas)
+    
+    # Now to finish cleaning up our case data! With 77000 rows and only ~365 days of data, it looks
+    # like they're reporting each case as a new line. That's kind of great as it's super granular,
+    # but for our purposes we can roll it up to LGAs and just see a count of how many in each LGA
+    # on each day. Here we go!
+    
+    # print(df.drop_duplicates(keep='first'))
+    
+    # Yup that looks much more reasonable, 8130 combinations. So let's group them together, reset
+    # the index and then merge with our LGA shape file. Note that in this case I'm adding a dummy
+    # column 'count' with values all 1 (as they're each 1 case) so groupby has something to work
+    # from.
+    
+    df['count'] = 1
+    df = df.groupby(['notification_date','lga_code19','lga_name19'])[['count']].count()
+    # print(df)
+    df = df.reset_index(drop=False)
+    # print(df)
+    
+    df = df.sort_values(by='notification_date', ascending=True)
+    
+    # max_color is interesting since we're splitting the plot into two. There's
+    # an argument to be made that max_color should be different for Sydney and
+    # regional, but I've experienced that argument with online people and it's not
+    # fun. Basically it'd look deceptive to have, say the Sydney LGA and the Unincorporated
+    # NSW LGA look the same, even though they could have very different numbers of COVID
+    # cases.
+    
+    return df, lgas
+    
 
 
 # We can now generate our plots, but it's tricky! My initial attempt essentially joined
@@ -133,37 +140,95 @@ dates = dates[:1]
 # To fix this, we have to merge on each date and then fillna as 0, so the other LGAs
 # still appear.
 
-# fig, (ax1, ax2) = plt.subplots(ncols=2, sharex=False, sharey=False)
-# fig.set_figsize = (20,8)
+# fig, (ax1, ax2) = plt.subplots(ncols=2, facecolor='black', sharex=False, sharey=False, figsize=(20,8))
+# # fig.set_figsize = (40,8)
+# # ax1.set_figsize = (40,8)
+# # ax2.set_figsize = (20,8)
+# fig.tight_layout()
+# # plt.figure(facecolor='black')
+# ax1.set_title('Regional', y=0, color='white')
+# ax2.set_title('Sydney', y=0, color='white')
+# ax1.set_axis_off()
+# ax2.set_axis_off()
+# plt.subplots_adjust(wspace=-0.4)
+# ax1.set_xlim(140, 154)
 # ax1.set_xlim(140,154)
 # ax2.set_xlim(149,152)
 # ax2.set_ylim(33,34)
 
-for i in dates:
-    print(i)
+def visualise(df, lgas, i):
+    print(f'{idx}/{len(dates)}: {i}')
     temp = df.loc[df['notification_date'] == i]
     merged = pd.merge(lgas, temp, how='left', right_on='lga_code19', left_on='LGA_CODE21')
     merged['count'] = merged['count'].fillna(0)
-    ax = merged.plot(column='count', 
-                    cmap='Reds', 
-                    vmax=max_color,
-                    edgecolor='black',
-                    figsize=(20,8))
-    # merged.plot(column='count', 
-    #             cmap='Reds', 
-    #             vmax=max_color,
-    #             edgecolor='black',
-    #             ax=ax2)
-    # ax.set_axis_off()
-    ax.set_xlim(150,152)
-    ax.set_ylim(35,33)
-    # ax.set_xlim(140,154)
-    ax.set_title(f'{i}')
-    # fig.set_title(i)
-    plt.savefig(f'{i}.jpg')
+    # print(merged)
+    syd = merged.loc[merged['Region'] == 'Sydney']
+    reg = merged.loc[merged['Region'] == 'Regional']
+    
+    fig, (ax1, ax2) = plt.subplots(ncols=2, facecolor='black', sharex=False, sharey=False, figsize=(20,8))
+    fig.tight_layout()
+    ax1.set_title('Regional', y=0, color='white')
+    ax2.set_title('Sydney', y=0, color='white')
+    ax1.set_axis_off()
+    ax2.set_axis_off()
+    plt.subplots_adjust(wspace=-0.4)
+    ax1.set_xlim(140, 154)
+    
+    syd.plot(column='count',
+             cmap='plasma',
+             vmax=max_color,
+             edgecolor='black',
+             ax=ax2)
+    
+    reg.plot(column='count',
+             cmap='plasma',
+             vmax=max_color,
+             edgecolor='black',
+             ax=ax1)
+    
+    fig.suptitle(i, fontsize=16, color='white')
+    plt.savefig(f'./output/{i}.jpg')
+    plt.close('all')
 
 # Great! After merging we only lost about 400 rows, but checking the initial extract
 # it looks like a few rows didn't have LGAs, which is fine. An expansion might be to clean
 # this data a bit more rigorously, but what we have should suffice for now (and in the overall
 # dataset before being grouped, it's <1% of the total data available).
 
+# Extract a subset of the data. It's clear for our purposes (a map of COVID cases
+# in NSW) we don't really care about most of the data; our shape files are for
+# LGAs so we'll keep the LGA name field (and possibly the code in case name doesn't
+# line up), and we'll keep the date because this is going to be based on date. Otherwise
+# we don't need postcode/suburb, NSW Health have been kind enough to conform them to
+# LGAs already.
+# url = 'https://data.nsw.gov.au/data/api/3/action/datastore_search?resource_id=21304414-1ff1-4243-a5d2-f52778048b29&limit=10'  
+
+# extract_from_api(url)
+
+def create_gif():
+    images = []
+    for file_idx, filename in enumerate(os.listdir('./output')):
+        if filename[-4:] == '.jpg':
+            print(file_idx)
+            images.append(imageio.imread(f'./output/{filename}'))
+    imageio.mimsave('visualisation.gif', images, fps=40, subrectangles=True)
+
+
+# With our second query we've got only what we wanted and we've extracted all the data,
+# all 77,000 rows of it.
+# url = 'https://data.nsw.gov.au/data/api/3/action/datastore_search_sql?sql=SELECT%20notification_date,lga_code19,lga_name19%20from%20%2221304414-1ff1-4243-a5d2-f52778048b29%22%20'
+
+# df = extract_from_api(url)
+
+# df, lgas = clean(df)
+
+# max_color = df['count'].max()
+    
+# dates = list(df['notification_date'].unique())
+
+# for idx, date in enumerate(dates):
+#     if idx == 0:
+#         visualise(df, lgas, date)
+
+create_gif()
+# imageio.help('gif')
